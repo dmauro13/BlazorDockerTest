ea01d4105f8a1026b8b3815905d5ba2d24000663
d.mauro <d.mauro@DAMIANO-NB>
2024-01-25 20:56:09 +0100

    Test pubblicazione container docker

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorDockerTest.Pages
+{
+	public partial class Counter
+	{
+		private int currentCount = 0;
+		private string textToShow = "";
+
+
+		private void IncrementCount()
+		{
+			currentCount++;
+		}
+
+		private void ShowString()
+		{
+			textToShow = "Il push di docker funziona!";
+		}
+	}
+}
