f0c0f4a507295ed7740c914b31652003064b9752
dmauro13 <damiano.mauro@teaide.it>
2024-03-02 19:19:57 +0100

    ...

---
+++ Pages/Employee1.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDockerTest.Pages
+{
+    public class Employee1
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public DateOnly DOJ { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Pages/Employee3.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorDockerTest.Pages
+{
+    public class Employee3
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Designation { get; set; }
+        public DateOnly DOJ { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
